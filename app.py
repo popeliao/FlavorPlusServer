7c6a45976e2ac4762c5d2fa3620a4826f772eafd
popeliao <popeliao@gmail.com>
2013-03-30 17:17:31 -0400

    initialize database everytime

--- app.py
+++ app.py
@@ -115,6 +115,6 @@ def retrive(query):
     return entry
 
 if __name__ == '__main__':
-    #init_db()
+    init_db()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
