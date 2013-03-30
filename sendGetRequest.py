73ad1f907067dcd48cab72a37cbc7af4cbac1503
popeliao <popeliao@gmail.com>
2013-03-30 17:34:31 -0400

    write get http request test in python

--- sendGetRequest.py
+++ sendGetRequest.py
@@ -1,6 +1,7 @@
 # coding: UTF-8
 import urllib2
-server = "http://localhost:5000/xml?query=date"
+#server = "http://localhost:5000/xml?query=date"
+server = "http://flavorplus.herokuapp.com/xml?query=date"
 req = urllib2.Request(server)
 response = urllib2.urlopen(req)
 print response.read()
